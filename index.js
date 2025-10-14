7d3016dd66d2755b8fa0740dbad67d7ee1138c1e
Harsh <saizalkhandelwal>
2025-10-14 23:05:21 +0530

    UPDATE

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const { faker } = require('@faker-js/faker');
+// this is used to aquire the faker
+// which is used the fake existing data about the real world
+const mysql2 = require('mysql2');
 
-let getRandomUser = () => {
+const connection = mysql2.createConnection(
+{
+   host:'localhost',
+   user:'root',
+   database:'STARUX_DATA',
+   password:"05012007"
+});
+try{
+connection.query('SHOW TABLES', function(err, results) {
+    if (err) 
+    {
+        throw err
+    } 
+    else{
+        console.log(results);
+    }
+    console.log("the try and catch task is completed");
+});
+}
+catch(e){
+    console.log("the program is not executing " , e);
+
+}
+finally{
+    console.log("finally the program is finshish");
+    connection.end();
+}
+
+let createRandomUser = () => {
     return {
-        Id: faker.string.uuid(),
+        userId: faker.string.uuid(),
         username: faker.internet.username(),
         email: faker.internet.email(),
-        ava: faker.image.avatar(),
-        password: faker.internet.password()
+        avatar: faker.image.avatar(),
+        password: faker.internet.password(),
+        birthdate: faker.date.birthdate(),
+        registeredAt: faker.date.past(),
     };
-}
-for (let i = 0; i <= 10; i++) {
-    console.log(getRandomUser());
-}
+};
+
+
